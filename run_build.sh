c93c7d92fc5299676558b09a1bef3ac20a2d7757
Fei Luo <axl.luo@gmail.com>
2026-02-11 21:11:33 +1300

    Update run_build.sh to remove specific stamps for external-dt, optee-os, and tf-a to force rebuild. Modify device tree overlay to enable USART6 on STM32MP257F-DK, adjusting resource assignments and statuses in the RIFSC configuration.

--- run_build.sh
+++ run_build.sh
@@ -2,11 +2,10 @@
 cd /home/felux/Projects/st/openstlinux
 DISTRO=openstlinux-weston MACHINE=stm32mp25-disco source layers/meta-st/scripts/envsetup.sh
 
-# Fix: sysroots-components was deleted, need to re-run both
-# do_populate_sysroot (creates sysroots-components) and
-# do_prepare_recipe_sysroot (links them into recipe workdirs)
-echo "Removing sysroot-related stamps to force repopulation from sstate..."
-find tmp-glibc/stamps -name '*do_populate_sysroot*' -delete 2>/dev/null
-find tmp-glibc/stamps -name '*do_prepare_recipe_sysroot*' -delete 2>/dev/null
+# Force rebuild of external-dt and dependent firmware by removing stamps
+echo "Removing stamps to force rebuild of external-dt, optee-os, tf-a..."
+find tmp-glibc/stamps -path '*external-dt*' -delete 2>/dev/null
+find tmp-glibc/stamps -path '*optee-os-stm32mp*' -delete 2>/dev/null
+find tmp-glibc/stamps -path '*tf-a-stm32mp*' -delete 2>/dev/null
 
 BB_NUMBER_THREADS=8 PARALLEL_MAKE="-j8" bitbake mp257-st-ros-base
